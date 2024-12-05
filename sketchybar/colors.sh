d227765912573686749ce2ac67688fe9caf5969e
FKX_21 <76435480+FKX21@users.noreply.github.com>
2024-12-05 19:45:44 +0100

    sketchybar

---
+++ sketchybar/colors.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+export WHITE=0xff000000
+
+# -- Teal Scheme --
+#export BAR_COLOR=0xff001f30
+#export ITEM_BG_COLOR=0xff003547
+#export ACCENT_COLOR=0xff2cf9ed
+
+# -- Gray Scheme --
+# export BAR_COLOR=0xff101314
+# export ITEM_BG_COLOR=0xff353c3f
+# export ACCENT_COLOR=0xffffffff
+
+# -- Purple Scheme --
+# export BAR_COLOR=0xff140c42
+# export ITEM_BG_COLOR=0xff2b1c84
+# export ACCENT_COLOR=0xffeb46f9
+
+# -- Red Scheme ---
+# export BAR_COLOR=0xff23090e
+# export ITEM_BG_COLOR=0xff591221
+# export ACCENT_COLOR=0xffff2453
+
+# -- Blue Scheme --- 
+# export BAR_COLOR=0xff021254
+# export ITEM_BG_COLOR=0xff093aa8
+# export ACCENT_COLOR=0xff15bdf9
+
+# -- Green Scheme --
+# export BAR_COLOR=0xff003315
+# export ITEM_BG_COLOR=0xff008c39
+# export ACCENT_COLOR=0xff1dfca1
+
+
+# -- Orange Scheme --
+# export BAR_COLOR=0xff381c02
+# export ITEM_BG_COLOR=0xff99440a
+# export ACCENT_COLOR=0xfff97716
+
+# -- Yellow Scheme --
+# export BAR_COLOR=0xff2d2b02
+# export ITEM_BG_COLOR=0xff8e7e0a
+# export ACCENT_COLOR=0xfff7fc17
+
+# -- Mavs Scheme --
+# export BAR_COLOR=0xff0064b1
+# export ITEM_BG_COLOR=0xff162d50
+# export ACCENT_COLOR=0xffbbc4ca
+
+# -- TDF Scheme --
+# export BAR_COLOR=0xfffabb00
+# export ITEM_BG_COLOR=0xff000000
+# export ACCENT_COLOR=0xff000000
+
+# -- Pixel Scheme --
+export BAR_COLOR=0xffe3e0f7
+export ITEM_BG_COLOR=0xffe0b3e6
+export ACCENT_COLOR=0xff8299e6
